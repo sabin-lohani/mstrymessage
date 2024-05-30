cc7065611d8046e083725fa42ca0cec7a28cda51
Sabin Lohani <np03cs4s220104@heraldcollege.edu.np>
2024-05-30 19:37:28 +0545

    chore: Add route for verifying user with verification code

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,56 @@
+import dbConnect from "@/lib/dbConnect";
+import User from "@/models/user.models";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    const { username, code } = await request.json();
+    const decodedUsername = decodeURIComponent(username);
+
+    const user = await User.findOne({
+      username: decodedUsername,
+      verificationCode: code,
+    });
+
+    if (!user) {
+      return Response.json(
+        { success: false, message: "User not found" },
+        { status: 400 }
+      );
+    }
+
+    const isCodeValid = user.verifyCode === code;
+    const isCodeNotExpired = user.verifyCodeExpiry > new Date();
+
+    if (isCodeValid || isCodeNotExpired) {
+      user.isVerified = true;
+      await user.save();
+      return Response.json(
+        { success: true, message: "Account verified successfully" },
+        { status: 200 }
+      );
+    } else if (!isCodeNotExpired) {
+      return Response.json(
+        {
+          success: false,
+          message: "Verification code expired. Sign up again to get a new code",
+        },
+        { status: 400 }
+      );
+    }
+
+    return Response.json(
+      { success: false, message: "Invalid verification code" },
+      { status: 400 }
+    );
+  } catch (error) {
+    console.error("Error verifying user", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error verifying user",
+      },
+      { status: 500 }
+    );
+  }
+}
